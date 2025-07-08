b19ac361f73984ee9dfa731765b78cc1228ebf17
itsviv0 <vivekganeshs12@gmail.com>
2025-07-08 21:39:19 +0530

    Updated experience data.

--- src/data/experience.js
+++ src/data/experience.js
@@ -9,31 +9,22 @@ const experiences = [
             "Implemented Python automation to identify relevant stakeholders and send targeted email notifications, resulting in a 40% improvement in bug resolution times.",
             "Built an interactive front-end dashboard using Streamlit to visualize key bug statistics for stakeholders, resulting in a 40% improvement in bug response times and a 30% increase in traceability accuracy."
         ],
-        "skills": ["Python", "Streamlit", "Bugzilla APIs", "Postman", "HTML", "CSS"]
-    },
-    {
-        "title": "Data Engineer Intern",
-        "company": "Hinge Health",
-        "location": "Bengaluru, IN (On-site)",
-        "period": "Nov 2024 - March 2025",
-        "description": [
-        "Developed and maintained data pipelines using Databricks and Apache Airflow, improving data processing efficiency by 35%.",
-        "Collaborated with cross-functional teams to implement data solutions that supported business decision-making.",
-        "Implemented real-time analytics dashboards that increased stakeholder visibility into key metrics for GROWTH Team.",
-        "Optimized SQL queries resulting in 40% faster reporting dashboard performance."
-        ],
-        "skills": ["Databricks", "Apache Airflow", "Python", "SQL", "Datadog"]
+        "skills": ["Python", "Streamlit", "Bugzilla APIs", "Postman", "HTML", "Jenkins"]
     },
     {
         "title": "Full-Stack Software Engineer Intern",
         "company": "Hinge Health",
         "location": "Bengaluru, IN (On-site)",
-        "period": "Aug 2024 - Oct 2024",
+        "period": "Aug 2024 - March 2025",
         "description": [
         "Built responsive front-end interfaces using React and TypeScript, enhancing user experience for healthcare professionals on CareHub.",
-        "Participated in agile development cycles, contributing to sprint planning and retrospectives."
+        "Participated in agile development cycles, contributing to sprint planning and retrospectives.",
+        "Developed and maintained data pipelines using Databricks and Apache Airflow, improving data processing efficiency by 35%.",
+        "Collaborated with cross-functional teams to implement data solutions that supported business decision-making.",
+        "Implemented real-time analytics dashboards that increased stakeholder visibility into key metrics for GROWTH Team.",
+        "Optimized SQL queries resulting in 40% faster reporting dashboard performance."
         ],
-        "skills": ["Next.js", "TypeScript", "GraphQL", "Tailwind CSS"]
+        "skills": ["Next.js", "TypeScript", "GraphQL", "Tailwind CSS", "Databricks", "Apache Airflow", "Python", "SQL", "Datadog"]
     }
 ]
 
